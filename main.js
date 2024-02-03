c040dda6efe2c92ab134125f95eb79fd1004a489
osama youseff <oy6305616@gmail.com>
2024-02-03 20:53:18 +0200

    adjust timezone

--- main.js
+++ main.js
@@ -154,7 +154,6 @@ function getTimeNow(need12h) {
     var formattedTime = hours + ':' + (minutes < 10 ? '0' : '') + minutes + ':' + (seconds < 10 ? '0' : '') + seconds;
   }
 
-  console.log(formattedTime);
   return formattedTime;
 }
 
